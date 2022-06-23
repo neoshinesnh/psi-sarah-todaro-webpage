5497acfded58995be5d13139725ab1ee411ffee0
Elton Martins <eltonmartins@MBP-de-Elton.(none)>
2022-06-23 18:02:37 -0300

    compression package

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+const compression = require('compression');
 const express = require('express');
 const path = require('path');
+
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+
 app.use('/public', express.static('public'));
 app.use('/assets', express.static('assets'));
+
+// Compress all HTTP responses
+app.use(compression());
+
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
